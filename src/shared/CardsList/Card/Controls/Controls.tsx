38d21283d6fff88bcfe1e7fd2baf7932f57131ba
ampersander <mr.khan4ik@gmail.com>
2023-01-26 03:37:29 +0600

    KarmaRating working now

--- src/shared/CardsList/Card/Controls/Controls.tsx
+++ src/shared/CardsList/Card/Controls/Controls.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './controls.css';
 
 export function Controls() {
+const [rateCount, rateCountCalc] = useState(0);
   return (
 <div className={styles.controls}>
 	<div className={styles.carmaCounter}>
-		<button className={styles.carmaUp}>
+		<button className={styles.carmaUp} onClick={()=>rateCountCalc(rateCount+1)}>
 		<svg width="19" height="10" viewBox="0 0 19 10" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path d="M9.5 0L0 10H19L9.5 0Z" fill="#C4C4C4"/></svg>
 		</button>
-		<p className={styles.carmaNumber}>110</p>
-		<button className={styles.carmaDown}>
+		<p className={styles.carmaNumber}>{rateCount}</p>
+		<button className={styles.carmaDown} onClick={()=>rateCountCalc(rateCount-1)}>
 		<svg width="19" height="10" viewBox="0 0 19 10" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path d="M9.5 10L19 0L8.74228e-07 -1.66103e-06L9.5 10Z" fill="#C4C4C4"/>
 		</svg>
 		</button>
 	</div>
 	<div className={styles.comments}>
-		<button className={styles.commentsLogo}>
+		<button className={styles.commentsLogo} >
 			<svg width="34" height="16" viewBox="0 0 34 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path d="M12.75 1H1.41667C0.6375 1 0 1.6375 0 2.41667V10.9167C0 11.6958 0.6375 12.3333 1.41667 12.3333H11.3333L14.1667 15.1667V2.41667C14.1667 1.6375 13.5292 1 12.75 1ZM11.3333 9.5H2.83333V8.08333H11.3333V9.5ZM11.3333 7.375H2.83333V5.95833H11.3333V7.375ZM11.3333 5.25H2.83333V3.83333H11.3333V5.25Z" fill="#C4C4C4"/>
 			</svg>
